48f96055d26941e173b0eb1d0c2ecde77ac7c853
takumi-kasahara <205691956+takumi-kasahara@users.noreply.github.com>
2026-06-07 18:01:11 +0900

    feat: add scripts for comparing items by full path and size/date using WinMerge
    Signed-off-by: takumi-kasahara <205691956+takumi-kasahara@users.noreply.github.com>

--- Scripts/SendTo/CompareItemByFull/Main.js
+++ Scripts/SendTo/CompareItemByFull/Main.js
@@ -14,6 +14,6 @@ for (var i = 0; i < WScript.Arguments.Unnamed.Length; i++) {
 if (files.length == 1)
   WshShell.Run('WinMerge.exe /e /u /self-compare "' + files[0] + '"', 1, false);
 else if (2 <= files.length && files.length <= 3)
-  WshShell.Run('WinMerge.exe /e /u /x "' + files.join('" "') + '"', 1, false);
+  WshShell.Run('WinMerge.exe /e /u /x /m Full "' + files.join('" "') + '"', 1, false);
 else if (2 <= folders.length && folders.length <= 3)
-  WshShell.Run('WinMerge.exe /e /u /x /r "' + folders.join('" "') + '"', 1, false);
+  WshShell.Run('WinMerge.exe /e /u /x /m Full /r "' + folders.join('" "') + '"', 1, false);
---
+++ Scripts/SendTo/CompareItemBySizeDate/Main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var WshShell = new ActiveXObject('WScript.Shell');
+var FileSystemObject = new ActiveXObject('Scripting.FileSystemObject');
+var files = [];
+var folders = [];
+for (var i = 0; i < WScript.Arguments.Unnamed.Length; i++) {
+  var path = WScript.Arguments.Unnamed.Item(i);
+  if (FileSystemObject.FileExists(path))
+    files.push(path);
+  else if (FileSystemObject.FolderExists(path))
+    folders.push(path);
+}
+if (files.length == 1)
+  WshShell.Run('WinMerge.exe /e /u /self-compare "' + files[0] + '"', 1, false);
+else if (2 <= files.length && files.length <= 3)
+  WshShell.Run('WinMerge.exe /e /u /x /m SizeDate "' + files.join('" "') + '"', 1, false);
+else if (2 <= folders.length && folders.length <= 3)
+  WshShell.Run('WinMerge.exe /e /u /x /m SizeDate /r "' + folders.join('" "') + '"', 1, false);
