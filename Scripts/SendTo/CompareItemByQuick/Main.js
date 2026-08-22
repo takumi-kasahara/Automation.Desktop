af1eabbbe57ca620764b8a0f40deff642111be6d
takumi-kasahara <205691956+takumi-kasahara@users.noreply.github.com>
2026-08-22 20:20:38 +0900

    feat: add script to compare files and folders using WinMerge
    Signed-off-by: takumi-kasahara <205691956+takumi-kasahara@users.noreply.github.com>

---
+++ Scripts/SendTo/CompareItemByQuick/Main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var WshShell = new ActiveXObject('WScript.Shell');
+var FileSystemObject = new ActiveXObject('Scripting.FileSystemObject');
+var files = [];
+var folders = [];
+for (var i = 0; i < WScript.Arguments.length; i++) {
+  var path = WScript.Arguments.Item(i);
+  if (FileSystemObject.FileExists(path)) files.push(path);
+  else if (FileSystemObject.FolderExists(path)) folders.push(path);
+}
+var winmerge = 'WinMerge.exe /e /u /x /m Quick';
+if (files.length === 1)
+  WshShell.Run(winmerge + ' /self-compare "' + files[0] + '"', 1, false);
+else if (2 <= files.length && files.length <= 3)
+  WshShell.Run(winmerge + ' "' + files.join('" "') + '"', 1, false);
+else if (2 <= folders.length && folders.length <= 3)
+  WshShell.Run(winmerge + ' /r "' + folders.join('" "') + '"', 1, false);
