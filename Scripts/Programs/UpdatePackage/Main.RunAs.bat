35910e3dab18f84aee0cda4f19a193292a539fb4
takumi-kasahara <205691956+takumi-kasahara@users.noreply.github.com>
2026-09-06 10:30:00 +0900

    feat: add nuget update command to Main.RunAs.bat script
    Signed-off-by: takumi-kasahara <205691956+takumi-kasahara@users.noreply.github.com>

--- Scripts/Programs/UpdatePackage/Main.RunAs.bat
+++ Scripts/Programs/UpdatePackage/Main.RunAs.bat
@@ -20,6 +20,7 @@ powershell -NoLogo -NoProfile -Command "Update-Module -ErrorAction SilentlyConti
 powershell -NoLogo -NoProfile -Command "Update-Help -ErrorAction SilentlyContinue"
 where /q apm >nul 2>&1 && apm update --global --yes
 where /q dotnet >nul 2>&1 && dotnet tool update --global --all
+where /q nuget >nul 2>&1 && nuget update -Self
 where /q npx >nul 2>&1 && cmd /c npx npm-check-updates --global --upgrade
 where /q npm >nul 2>&1 && cmd /c npm update --global --no-fund
 
